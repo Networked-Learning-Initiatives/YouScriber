510bbbed036876d7c0066c9be8f1ea26fbbb2c35
Michael Stewart <tgm@vt.edu>
2014-06-13 11:04:50 -0400

    improve youtube directive

--- app/scripts/directives/youtube/youtube.js
+++ app/scripts/directives/youtube/youtube.js
@@ -1,18 +1,34 @@
-angular.module('youtubeapi',[]).directive('youtube', function($sce, $location, $interval) {
+angular.module('youtubeapi', []).directive('ysYoutube', function($sce, $location, $interval, $resource) {
   return {
-    restrict: 'EA',
+    restrict: 'E',
     scope: { 
       code: '=videoId',
       width: '=w',
       height: '=h',
       playerId: '=playerId',
-      time: '='
+      time: '=',
+      videoMetadata: '=metadata'
     },
     replace: true,
     templateUrl: 'scripts/directives/youtube/youtube.html',
-    compile: function(tElement, tAttrs) {
-      return function (scope, iElement, iAttrs, controller) {
-        var el = document.getElementById('youtube-player-' + scope.playerId);
+    link: function (scope, iElement, iAttrs, controller) {
+      var el = document.getElementById('youtube-player-' + scope.playerId);
+      // Lets Flash from another domain call JavaScript
+      scope.params = { allowScriptAccess: "always" };
+      // The element id of the Flash embed
+      scope.atts = { 
+        id: "ytPlayer-"+scope.playerId, 
+        data: "https://www.youtube.com/v/" + scope.code + "?version=3&enablejsapi=1&playerapiid="+scope.playerId,
+        width: scope.width,
+        height: scope.height
+      };
+
+      var Videos = $resource('http://gdata.youtube.com/feeds/api/videos/:videoid?v=2&alt=json', {query:'@query'});
+      scope.videoMetadata = Videos.get({videoid:scope.code});
+      
+      scope.$watch('code', function (newVal) {
+        $interval.cancel(scope.timer);
+        angular.element('#video-container').append(angular.element('<div id="swf-container"></div>'));
         // Lets Flash from another domain call JavaScript
         scope.params = { allowScriptAccess: "always" };
         // The element id of the Flash embed
@@ -22,34 +38,19 @@ angular.module('youtubeapi',[]).directive('youtube', function($sce, $location, $
           width: scope.width,
           height: scope.height
         };
-        
-        scope.$watch('code', function (newVal) {
-          $interval.cancel(scope.timer);
-          angular.element('#video-container').append(angular.element('<div id="swf-container"></div>'));
-          // Lets Flash from another domain call JavaScript
-          scope.params = { allowScriptAccess: "always" };
-          // The element id of the Flash embed
-          scope.atts = { 
-            id: "ytPlayer-"+scope.playerId, 
-            data: "https://www.youtube.com/v/" + scope.code + "?version=3&enablejsapi=1&playerapiid="+scope.playerId,
-            width: scope.width,
-            height: scope.height
-          };
 
-          $location.path(scope.code);
-
-          swfobject.createSWF(scope.atts, scope.params, "swf-container");
-          scope.timer = $interval(function(){
-            var el = $('#ytPlayer-'+scope.playerId)[0];
-            if (el && el.hasOwnProperty('getCurrentTime')){
-              var t = el.getCurrentTime();
-              if (t>0) {
-                scope.time = t;
-              }
+        swfobject.createSWF(scope.atts, scope.params, "swf-container");
+        scope.timer = $interval(function(){
+          console.log('timer went off');
+          var el = $('#ytPlayer-'+scope.playerId)[0];
+          if (el && el.hasOwnProperty('getCurrentTime')){
+            var t = el.getCurrentTime();
+            if (t>0) {
+              scope.time = t;
             }
-          }, 100);
-        });
-      }
+          }
+        }, 100);
+      });
     }
   };
 });
