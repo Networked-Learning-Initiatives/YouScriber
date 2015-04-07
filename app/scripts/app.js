bbbe31ef162f4c29d7e05b71862f9e19e468d03e
Tav0 <lv23@vt.edu>
2015-04-07 12:27:56 -0400

    fix closing settings window when click back

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,88 +1,101 @@
 'use strict';
 
 angular.module('youScriberApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute',
-  'youtubeapi',
-  'xeditable',
-  'ui.bootstrap',
-  'angular-md5',
-  'ngEnter',
-  'ui.router'
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute',
+    'youtubeapi',
+    'xeditable',
+    'ui.bootstrap',
+    'angular-md5',
+    'ngEnter',
+    'ui.router'
 ])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/dash');
-    $stateProvider
-      .state('dash', {
-        url:'/dash', 
-        templateUrl: 'views/dash.html',
-        controller: 'DashCtrl'
-      })
-      .state('video', {
-        url:'/video/:videoId', 
-        templateUrl: 'views/video.html',
-        controller: 'VideoCtrl'
-      })
-        .state('video.comments', {
-          // params: [],
-          url:'/comments',
-          templateUrl: 'views/comments.html',
-          controller: 'CommentsCtrl'
-        })
-          .state('video.comments.settings', {
-            url: '/settings',
-            onEnter: ['$stateParams', '$state', '$modal', '$resource', 'Videos', '$http', 'User', function($stateParams, $state, $modal, $resource, Videos, $http, User) {
-              $modal.open({
-                  size: 'lg',
-                  templateUrl: "views/video/settings.html",
-                  resolve: {
-                    video: function() { return Videos.getVideo($stateParams.videoId); }
-                  },
-                  controller: 'VideoSettings'
-              }).result.finally(function() {
-                  $state.go('^');
-              });
-            }]
-          })
-        .state('video.comment', {
-          // params: [],
-          url:'/comments/:commentId',
-          templateUrl: 'views/comments.html',
-          controller: 'CommentsCtrl'
-        })
-      .state('register', {
-        url:'/register', 
-        templateUrl: 'views/registration.html',
-        controller: 'RegistrationCtrl'
-      })
-      .state('login', {
-        url:'/login', 
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .state('registerOrg', {
-        url:'/register/org', 
-        templateUrl: 'views/org-registration.html',
-        controller: 'OrganizationRegistrationCtrl'
-      })
-      .state('registerGroup', {
-        url:'/register/group', 
-        templateUrl: 'views/group-registration.html',
-        controller: 'GroupRegistrationCtrl'
-      })
-      .state('org', {
-        url:'/org/:orgId/register/group', 
-        templateUrl: 'views/group-registration.html',
-        controller: 'GroupRegistrationCtrl'
-      })
-      .state('manage', {
-        url:'/manage', 
-        templateUrl: 'views/manage.html',
-        controller: 'ManageCtrl'
-      })
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/dash');
+  var modalInstance;
+  $stateProvider
+    .state('dash', {
+      url:'/dash', 
+      templateUrl: 'views/dash.html',
+      controller: 'DashCtrl'
+    })
+  .state('video', {
+    url:'/video/:videoId', 
+    templateUrl: 'views/video.html',
+    controller: 'VideoCtrl'
   })
-  .run(function(editableOptions) {
-    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-  });
+  .state('video.comments', {
+    // params: [],
+    url:'/comments',
+    templateUrl: 'views/comments.html',
+    controller: 'CommentsCtrl'
+  })
+  .state('video.comments.settings', {
+    url: '/settings',
+    onEnter: ['$stateParams', '$state', '$modal', '$resource', 'Videos', 
+    '$http', 'User', function($stateParams, $state, $modal,
+        $resource, Videos, $http, User) {
+      modalInstance = $modal.open({
+        size: 'lg',
+        templateUrl: "views/video/settings.html",
+        resolve: {
+          video: function() { 
+            return Videos.getVideo($stateParams.videoId); }
+        },
+        controller: 'VideoSettings'
+      });
+      modalInstance.result['finally'](function() {
+        modalInstance = null;
+        if($state.$current.name === stateName){
+          $state.go('ˆ');
+        }
+      });
+    }],
+    onExit: function() {
+      if(modalInstance) {
+        modalInstance.close();
+      }
+    }
+  })
+  .state('video.comment', {
+    // params: [],
+    url:'/comments/:commentId',
+    templateUrl: 'views/comments.html',
+    controller: 'CommentsCtrl'
+  })
+  .state('register', {
+    url:'/register', 
+    templateUrl: 'views/registration.html',
+    controller: 'RegistrationCtrl'
+  })
+  .state('login', {
+    url:'/login', 
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('registerOrg', {
+    url:'/register/org', 
+    templateUrl: 'views/org-registration.html',
+    controller: 'OrganizationRegistrationCtrl'
+  })
+  .state('registerGroup', {
+    url:'/register/group', 
+    templateUrl: 'views/group-registration.html',
+    controller: 'GroupRegistrationCtrl'
+  })
+  .state('org', {
+    url:'/org/:orgId/register/group', 
+    templateUrl: 'views/group-registration.html',
+    controller: 'GroupRegistrationCtrl'
+  })
+  .state('manage', {
+    url:'/manage', 
+    templateUrl: 'views/manage.html',
+    controller: 'ManageCtrl'
+  })
+})
+.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
