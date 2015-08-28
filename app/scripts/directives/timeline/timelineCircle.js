8ddfa112eaa68cec3434ad2bd347bdc6835109d0
Michael Stewart <tgm@vt.edu>
2015-08-27 21:13:30 -0400

    try to fix timeline circle

--- app/scripts/directives/timeline/timelineCircle.js
+++ app/scripts/directives/timeline/timelineCircle.js
@@ -10,32 +10,39 @@ angular.module('youScriberApp').directive('timelineCircle', function($document, 
     templateUrl: 'views/directives/timelineCircle.html',
     link: function (scope, iElement, iAttrs, controller) {
 
-      var startX = 0, x = 0;
+      // var startX = 0, x = 0;
 
       var isDragging = false;
 
       scope.style = {
-        top: 0,
-        left: '50px',
+        top: '0px',
+        // left: '40px',
       };
+      
+      $document.on('mousemove', mousemove);
+      $document.on('mouseup', mouseup);
 
       iElement.on('mousedown', function(event) {
-      // Prevent default dragging of selected content
-      isDragging = true;
+        // Prevent default dragging of selected content
+        isDragging = true;
         event.preventDefault();
-        startX = event.pageX - x;
+        // startX = event.offsetX - x;
+        // console.log('just set startX to', startX);
         // startY = event.pageY - y;
-        $document.on('mousemove', mousemove);
-        $document.on('mouseup', mouseup);
+        
       });
 
       function mousemove(event) {
-        // y = event.pageY - startY;
-        x = event.pageX - startX;
-        iElement.css({
-          // top: y + 'px',
-          left:  x + 'px'
-        });
+        if (isDragging) {
+          // y = event.pageY - startY;
+          // console.log(event.offsetX);
+          x = event.offsetX;
+          console.log(x + 'px');
+          iElement.css({
+            // top: y + 'px',
+            left:  x + 'px'
+          });
+        }
       }
 
       function mouseup() {
@@ -68,16 +75,16 @@ angular.module('youScriberApp').directive('timelineCircle', function($document, 
       //   var result = 0<=delta && delta < 1.3;
       //   return result;
       // };
-      scope.$watch('time', function(newValue, oldValue) {
-        // console.log(newValue);
-        // console.log(scope.duration);
-        // console.log(newValue/scope.duration);
-        // console.log(iElement.position());
-        // iElement.position(newValue, 0);
-        if (!isDragging) {
-          scope.style.left=newValue/scope.duration*scope.width+'px';
-        }
-      });
+      // scope.$watch('time', function(newValue, oldValue) {
+      //   // console.log(newValue);
+      //   // console.log(scope.duration);
+      //   // console.log(newValue/scope.duration);
+      //   // console.log(iElement.position());
+      //   // iElement.position(newValue, 0);
+      //   if (!isDragging) {
+      //     scope.style.left=newValue/scope.duration*scope.width+'px';
+      //   }
+      // });
     }
   };
 });
